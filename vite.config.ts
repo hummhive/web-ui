e708852fc4cb491e3a59929bf1db2dc4aad396f1
Pedro Roman <pedro@fusionbreak.com>
2025-02-24 17:24:18 -0400

    fix: crypto.createHash issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    dedupe: ['react', 'react-dom'],
+    dedupe: ["react", "react-dom"],
+    alias: {
+      crypto: "crypto-browserify",
+    },
   },
   optimizeDeps: {
-    include: ['lucide-react']
-  }
+    include: ["lucide-react"],
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+      ],
+    },
+  },
 });
