f6f70ff34a19777a13b57ea1052c29ae7ba2627f
Pedro Roman <pedro@fusionbreak.com>
2025-02-24 17:25:21 -0400

    feat: Adds post page with story data

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,33 +1,53 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
-import { useWebSocket } from '../hooks/useWebSocket';
+import Builder from "../Builder";
 
-const Post: React.FC = () => {
-  const { id } = useParams();
-  const { data: posts } = useWebSocket('ws://localhost:8080');
-  const post = posts[0];
+interface WebSocketData {
+  data: any[];
+  agentInfo: any;
+  isConnected: boolean;
+  error: any;
+}
 
-  if (!post) return null;
+const Post: React.FC<WebSocketData> = ({ data, isConnected, error  }) => {
+  const { id } = useParams<{ id: string }>();
+  if (error) {
+    return <div>❌ Error: {error}</div>;
+  }
+
+  if (!isConnected) {
+    return <div>Connecting to Holo… Please wait.</div>;
+  }
+
+  const post = data.find((post) => {
+    try {
+      const postObject = JSON.parse(post);
+      return postObject.storyId === id;
+    } catch (error) {
+      console.error('Error parsing post:', error);
+      return false;
+    }
+  });
+
+  if (!post) return <div>Post not found</div>;
+
+  const postObject = JSON.parse(post);
 
   return (
     <>
       <Helmet>
-        <title>{post.title} - HummHive</title>
-        <meta name="description" content={post.excerpt} />
+        <title>{postObject.title} - HummHive</title>
+        <meta name="description" content={postObject.excerpt} />
       </Helmet>
 
       <article className="prose dark:prose-invert max-w-none">
-        <h1 className="font-serif mb-4">{post.title}</h1>
-        <div className="text-sm text-stone-400 dark:text-stone-500 mb-12 flex items-center gap-2 -mt-4">
-          <span>{post.date}</span>
-          <span>·</span>
-          <span>{post.readingTime}</span>
-          <span>·</span>
-          <span>{post.author}</span>
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+        <h1 className="font-serif mb-4">{postObject.title}</h1>
+        {JSON.parse(postObject.body).map((element, i) => (
+              <Builder key={i} element={element} />
+            ))}
       </article>
+      {error && <p>Error: {error.message}</p>}
     </>
   );
 };
