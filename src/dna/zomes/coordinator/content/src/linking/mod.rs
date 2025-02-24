c1c9bda577b4e1baf6e47f14234b9e5d347449d8
Pedro Roman <pedro@fusionbreak.com>
2025-02-24 17:23:09 -0400

    chore: Remove DNA directory

--- src/dna/zomes/coordinator/content/src/linking/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod acl_links;
-pub mod dynamic_links;
-pub mod hive_link;
-pub mod humm_content_id_link;
-pub mod time_indexed_links;
--- src/dna/zomes/integrity/content/src/globals.rs
+++
@@ -1,1 +0,0 @@
-pub const ENCRYPTED_CONTENT_TIME_INDEX: &'static str = "encrypted_content_time";
